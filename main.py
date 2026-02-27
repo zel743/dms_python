a784a886e1b6fd0c71fd116e21e2cfcbd9d8d0f6
zel743 <196152738+zel743@users.noreply.github.com>
2026-02-26 20:35:01 -0600

    test commit

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import sys
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QLabel,
+    QMainWindow,
+    QStatusBar,
+    QToolBar,
+)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -7,10 +16,18 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Qt en VS Code")
         self.label = QLabel("¡Hola desde PyQt6!")
         self.setCentralWidget(self.label)
-        self.resize(400, 300)
+        self.resize(1512, 982)
+
+        toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(40, 40))
+        self.addToolBar(toolbar)
+        self.city = self.ui.comboBoxCity.currentText()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
+
+
